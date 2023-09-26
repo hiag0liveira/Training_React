f201de86ac77f0217d44376f6b05eab6b26dc96f
Hiago Mendes <hiagophd@gmail.com>
2023-09-26 19:14:57 -0300

    Iniciando uma aplicação no React

---
+++ 01-Aplicando_React/index.jsx
@@ -0,0 +1,13 @@
+
+function PrimeiroButton(){
+    return <button>Botão</button>
+}
+
+function Container(){
+    return(
+        <div className="">
+            <h1></h1>
+            <p></p>
+        </div>
+    )
+}
