8f6464330ce12afea98bb22f9746503207e13697
Hiago Mendes <hiagophd@gmail.com>
2023-10-04 20:54:00 -0300

    Gerador de senha

---
+++ 11-Gerador_Senha/Gerador de Senha/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import Input from "./components/Input"
+
+function App() {
+  const [password, setPassword] = useState("")
+  const [passwordSize, setPasswordSize] = useState(12)
+	const [copyText, setCopyText] = useState("Copiar")
+
+	function generate() {
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    let newPassword = ""
+    for (let i = 0; i < passwordSize; i++) {
+      const position = Math.floor(Math.random() * characters.length)
+      newPassword += characters[position]
+    }
+    setPassword(newPassword)
+		setCopyText("Copiar")
+  }
+
+	function copyToClipboard() {
+    window.navigator.clipboard.writeText(password)
+    setCopyText("Copiado!")
+  }
+  return (
+    <div>
+      <h1>Gerador de senhas</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho: </label>
+        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+      </div>
+      <button onClick={generate}>Gerar senha de {passwordSize} caracteres</button>
+      <button onClick={copyToClipboard}>{copyText}</button>
+      <div>Sua senha segura: {password}</div>
+    </div>
+  )
+}
+
+export default App
