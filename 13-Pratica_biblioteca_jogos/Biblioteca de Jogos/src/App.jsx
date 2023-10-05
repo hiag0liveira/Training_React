162742c1ee643bdf868059e0298d17ee40c7f71a
Hiago Mendes <hiagophd@gmail.com>
2023-10-05 20:42:33 -0300

    Biblioteca de Jogos

--- 13-Pratica_biblioteca_jogos/Biblioteca de Jogos/src/App.jsx
+++ 13-Pratica_biblioteca_jogos/Biblioteca de Jogos/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NewGameForm from "./components/NewGameForm"
+import Game from "./components/Game"
+import useGameCollection from "./hooks/useGameCollection"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { games, addGame, removeGame } = useGameCollection()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Biblioteca de Jogos</h1>
+      <NewGameForm addGame={addGame} />
+      <div className="games">
+        {games.length > 0 ? 
+          games.map((game) => (
+          <Game
+            key={game.id}
+            title={game.title}
+            cover={game.cover}
+            onRemove={() => removeGame(game.id)}
+          />))
+        :
+          (<h2 style={{ margin: "4 rem auto"}}>
+            Não tem jogos Adicionados, adicione algum jogo!
+          </h2>)
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
