0827ab6f8cb921c8891398e0c2fb4ec90e2edb05
Hiago Mendes <hiagophd@gmail.com>
2023-10-04 21:21:21 -0300

    Renderizacao condicional

--- 11-Gerador_Senha/Gerador de Senha/src/App.jsx
+++ 11-Gerador_Senha/Gerador de Senha/src/App.jsx
@@ -3,8 +3,11 @@ import Input from "./components/Input"
 
 function App() {
   const [password, setPassword] = useState("")
-  const [passwordSize, setPasswordSize] = useState(12)
+  const [customSize, setCustomSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
 	const [copyText, setCopyText] = useState("Copiar")
+
+  const passwordSize = showInput ? customSize : 8
 
 	function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
@@ -22,13 +25,24 @@ function App() {
     setCopyText("Copiado!")
   }
   return (
-    <div>
+    <div className = "app">
       <h1>Gerador de senhas</h1>
       <div>
-        <label htmlFor="passwordSize">Tamanho: </label>
-        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+        <label htmlFor="showInput">Customizar tamanho:</label>
+        <input
+          type="checkbox"
+          id="showInput"
+          value={showInput}
+          onChange={() => setShowInput(currentState => !currentState)}
+        />
       </div>
-      <button onClick={generate}>Gerar senha de {passwordSize} caracteres</button>
+      {showInput && (
+        <div>
+          <label htmlFor="passwordSize">Tamanho: </label>
+          <Input passwordSize={customSize} setPasswordSize={setCustomSize} />
+        </div>
+      )}
+      <button onClick={generate}>Gerar senha de {passwordSize} caracteres!</button>
       <button onClick={copyToClipboard}>{copyText}</button>
       <div>Sua senha segura: {password}</div>
     </div>
--- 11-Gerador_Senha/Gerador de Senha/src/components/Input.jsx
+++ 11-Gerador_Senha/Gerador de Senha/src/components/Input.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 Input.propTypes = {
     passwordSize: PropTypes.number.isRequired,
-    setPasswordSize: PropTypes.func.isre
+    setPasswordSize: PropTypes.func
 }
 
 export default function Input(props) {
@@ -14,7 +14,7 @@ export default function Input(props) {
             id="passwordSize"
             min={1}
             value={props.passwordSize}
-            onChange={(ev) => props.setPasswordSize(ev.target.value)}
+            onChange={(ev) => props.setPasswordSize(+ev.target.value)}
         />
     )
 }
